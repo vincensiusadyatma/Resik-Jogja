850dd1e7eb7a31d9aae3e030134da283d94e699e
KrishnanditoLksn <krislaksana123@gmail.com>
2025-09-29 23:09:02 +0700

    feat: update routes to serve main and about us views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('testing');
-});
+    return view('main.main');
+})->name('main');
+
+
+
+Route::get('/about', function () {
+    return view('main.about_us_main');
+})->name('about');
