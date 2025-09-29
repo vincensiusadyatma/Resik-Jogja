76f213bd56a49c4061e22936b9ec1be0ed3553d5
vincenciusadyatma <adyatmav@gmail.com>
2025-09-30 04:39:37 +0700

    feat: create auth system release 1.0.0

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('testing');
+})->name('main');
+
+Route::prefix('auth')->middleware('guest')->group(function () {
+    Route::get('/register/handle', [AuthController::class, 'handleRegister'])->name('handle-register');
+    Route::get('/login/handle', [AuthController::class, 'handleLogin'])->name('handle-login');
 });
+
+Route::get('/auth/logout', [AuthController::class, 'handleLogout'])->name('handle-logout');
