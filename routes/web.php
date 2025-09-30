913dfddc98c6aa9fc032b5e54ad283763c5987fe
vincenciusadyatma <adyatmav@gmail.com>
2025-09-30 10:10:12 +0700

    fix: change request method auth from get to post

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/', function () {
 })->name('main');
 
 Route::prefix('auth')->middleware('guest')->group(function () {
-    Route::get('/register/handle', [AuthController::class, 'handleRegister'])->name('handle-register');
-    Route::get('/login/handle', [AuthController::class, 'handleLogin'])->name('handle-login');
+    Route::post('/register/handle', [AuthController::class, 'handleRegister'])->name('handle-register');
+    Route::post('/login/handle', [AuthController::class, 'handleLogin'])->name('handle-login');
 });
 
-Route::get('/auth/logout', [AuthController::class, 'handleLogout'])->name('handle-logout');
+Route::post('/auth/logout', [AuthController::class, 'handleLogout'])->name('handle-logout');
