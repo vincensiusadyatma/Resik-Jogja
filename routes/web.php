16243711dda32236a081ecc3bfdd97e5956fe455
KrishnanditoLksn <krislaksana123@gmail.com>
2025-09-30 14:30:34 +0700

    delete: remove about route from web.php

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,11 @@ Route::get('/', function () {
 })->name('main');
 
 
-
-Route::get('/about', function () {
-    return view('main.about_us_main');
-})->name('about');
-
-
 Route::get('/artikel', function () {
     return view('main.articles_page_main');
 })->name('artikel');
+
+
+Route::get('/home', function () {
+    return view('main.main');
+})->name('home');
