8a815e5785108f5910849713e0c27dac1b5ec879
Duaarr <nuelivanimanuel@gmail.com>
2025-09-30 15:56:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('testing');
 });
+
+Route::get('/login', function () {
+    return view('.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('.register');
+})->name('register');
